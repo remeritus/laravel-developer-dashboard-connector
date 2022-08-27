9d246195a047f8fc06fc3c46bff45b9106c95b85
Richard Sihm <development@remerit.us>
2022-08-27 06:36:49 +0200

    refactoring tests

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -5,24 +5,27 @@ use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 
 it('will fail without token', function () {
-    $response = Http::get(route('connect'));
-    expect($response->status())->toBe(Response::HTTP_UNAUTHORIZED);
+
+    $response = $this->get('ldd/connect');
+
+    expect($response->getStatusCode())
+        ->toBe(Response::HTTP_UNAUTHORIZED);
 });
 
 it('will fail with wrong token', function () {
-    putenv('DEVELOPER_DASHBOARD_TOKEN=CorrectToken');
 
-    $response = Http::withToken('WrongToken')
-                ->get(route('connect'));
+    $response = $this->withToken('WrongToken')
+        ->get('ldd/connect');
 
-    expect($response->status())->toBe(Response::HTTP_UNAUTHORIZED);
+    expect($response->getStatusCode())
+        ->toBe(Response::HTTP_UNAUTHORIZED);
 });
 
 it('will succeed with correct token', function () {
-    putenv('DEVELOPER_DASHBOARD_TOKEN=CorrectToken');
 
-    $response = Http::withToken('CorrectToken')
-        ->get(route('connect'));
+    $response = $this->withToken('CorrectToken')
+        ->get('ldd/connect');
 
-    expect($response->status())->toBe(Response::HTTP_OK);
+    expect($response->getStatusCode())
+        ->toBe(Response::HTTP_OK);
 });
