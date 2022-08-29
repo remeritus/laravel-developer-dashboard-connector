387146de73e67610a7897f5abd9dd0d76c757517
Richard Sihm <development@remerit.us>
2022-08-29 21:45:41 +0200

    wip

--- tests/AuthenticationTest.php
+++ tests/AuthenticationTest.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Response;
 
 it('will fail without token', function () {
     $response = $this->get('ldd/connect');
-
+    var_dump($response);
     expect($response->getStatusCode())
         ->toBe(Response::HTTP_UNAUTHORIZED);
 });
