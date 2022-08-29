20b65c1ea1219d6a153146dd233988cb650fe83a
Richard Sihm <development@remerit.us>
2022-08-29 22:35:57 +0200

    wip

--- src/Controllers/DeveloperDashboardController.php
+++ src/Controllers/DeveloperDashboardController.php
@@ -11,7 +11,7 @@ class DeveloperDashboardController
     public function connect(Request $request): string
     {
         if ($this->authorize($request)->denied()) {
-            return \response()->setStatusCode(401, 'Token mismatch');
+            return \response()->json(['message' => 'Token mismatch.'], 401);
         }
 
         return json_encode($this->getLaravelData());
